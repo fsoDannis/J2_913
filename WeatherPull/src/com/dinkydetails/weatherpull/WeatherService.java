89a46f694ba595c4ea33c214eaab5527a7d29ed5
Dan <Dan@Dinkydetails.com>
2013-09-04 22:45:12 -0400

    Service (Countdown) is working

--- WeatherPull/src/com/dinkydetails/weatherpull/WeatherService.java
+++ WeatherPull/src/com/dinkydetails/weatherpull/WeatherService.java
@@ -1,7 +1,12 @@
 package com.dinkydetails.weatherpull;
 
+import android.app.Activity;
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
 
 public class WeatherService extends IntentService{
@@ -17,8 +22,19 @@ public class WeatherService extends IntentService{
 	@Override
 	protected void onHandleIntent(Intent intent) {
 		// TODO Auto-generated method stub
+		Log.i("onHandleIntent", "started");
+		
+		Bundle extras = intent.getExtras();
+		Messenger messenger = (Messenger) extras.get(MESSENGER_KEY);
+		String timer = extras.getString(TIME_KEY);
+		
 		
 		int countdown = 10;
+		try{
+			countdown = Integer.parseInt(timer);
+		}catch (Exception e){
+			Log.e("onHandleIntent", e.getMessage().toString());
+		}
 		
 		while (countdown > 0)
 		{
@@ -27,10 +43,23 @@ public class WeatherService extends IntentService{
 			}catch (InterruptedException e){
 				Log.e("Sleep", e.getMessage().toString());
 				e.printStackTrace();
-				
 			}
+			countdown --;
+			
+			Log.i("onHandleIntent", "Counter= "+ String.valueOf(countdown));
 		}
+		Log.i("onHandleIntent", "Counter is Done");
 		
+		Message message= Message.obtain();
+		message.arg1 = Activity.RESULT_OK;
+		message.obj ="Service is Done";
+		
+		try {
+			messenger.send(message);
+		} catch (RemoteException e) {
+			Log.e("onHandleIntent", e.getMessage().toString());
+			e.printStackTrace();
+		}
 	}
 
 }
