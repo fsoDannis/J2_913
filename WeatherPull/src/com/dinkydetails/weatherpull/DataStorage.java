40415ce9cc47fbc67a2c2e05f6446a9623514679
Dan <Dan@Dinkydetails.com>
2013-09-06 22:04:12 -0400

    Creating more Code… Working on the Handler Again

--- WeatherPull/src/com/dinkydetails/weatherpull/DataStorage.java
+++ WeatherPull/src/com/dinkydetails/weatherpull/DataStorage.java
@@ -1,5 +1,14 @@
 package com.dinkydetails.weatherpull;
 
+import android.content.Context;
+
 public class DataStorage {
+
+	public static void storeStringFile(Context baseContext, String string,
+			String string2) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
