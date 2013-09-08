6adc4e94db1ef0aca1c06db9bbc5a333dd497dd5
Dan <Dan@Dinkydetails.com>
2013-09-08 08:18:06 -0400

    Whoops… old XML too…

--- WeatherPull/gen/com/dinkydetails/weatherpull/R.java
+++ WeatherPull/gen/com/dinkydetails/weatherpull/R.java
@@ -27,7 +27,7 @@ public final class R {
         public static final int action_settings=0x7f080003;
         public static final int buttonSearch=0x7f080001;
         public static final int editTextZipCode=0x7f080000;
-        public static final int list=0x7f080002;
+        public static final int resultTextView=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
