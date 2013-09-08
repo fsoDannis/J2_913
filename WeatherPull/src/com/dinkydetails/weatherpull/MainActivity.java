ddce8710635d135ca5b12b07de5202116375670a
Dan <Dan@Dinkydetails.com>
2013-09-07 20:32:04 -0400

    Fixed some of my "e" variables

--- WeatherPull/src/com/dinkydetails/weatherpull/MainActivity.java
+++ WeatherPull/src/com/dinkydetails/weatherpull/MainActivity.java
@@ -1,5 +1,12 @@
 package com.dinkydetails.weatherpull;
 
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.R;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -12,6 +19,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -49,40 +57,103 @@ public class MainActivity extends Activity implements OnClickListener{
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		
+		
+		
 		if (editTextZipCode.getText().toString().length()==0){
 			Toast.makeText(this, "Please Enter a Valid Zip Code!", Toast.LENGTH_LONG).show();
 			return;
 		}
-		Handler countdownHandler = new Handler(){
+		
+		
+		Handler myDataHandler = new Handler(){
 
 			@Override
 			public void handleMessage(Message msg) {
 				// TODO Auto-generated method stub
+			Log.i("SERVICE", "handlemessage() called");
+			String response = null;	
 			
-				String response = null;	
 			if (msg.arg1 == RESULT_OK && msg.obj !=null ){
 				try{
 					response = (String) msg.obj; 
+					Log.i("JSON DATA", response);
+					
+					//search_string may be ZipCode
+					displayData(search_string);
+					
 				}
 				catch (Exception e){
-					Log.e("handleMessage", e.getMessage().toString());
+					Log.e("JSON Response", e.toString());
 				}
-				
-				resultTextView.setText(response);
-				//resultTextView.setTextColor(getResources().getColor(R.color.green));
 				}
 			}
 		};
 		
-		Messenger countdownMessenger = new Messenger(countdownHandler);
+		Messenger myDataMessenger = new Messenger(myDataHandler);
 		
-		Intent startCountdownIntent = new Intent (this, WeatherService.class);
-		startCountdownIntent.putExtra(WeatherService.MESSENGER_KEY, countdownMessenger);
-		startCountdownIntent.putExtra(WeatherService.SEARCH_STRING, this.editTextZipCode.getText().toString());
-		startService(startCountdownIntent);
+		Intent startDataServiceIntent = new Intent (getBaseContext(), WeatherService.class);
+		startDataServiceIntent.putExtra("messenger", myDataMessenger);
+		startDataServiceIntent.putExtra("user_input", search_string);
+		startService(startDataServiceIntent);
 		
 		resultTextView.setText("Waiting....");
 		
-	}
+		}
+	});
     
+	if (savedInstanceState  != null){
+		Log.i("SAVE INSTANCE", "saving instance");
+		
+		myList = (ArrayList<HashMap<String,String>>) savedInstanceState.getSerializable("saved");
+		
+		if (myList != null){
+			SimpleAdapter adapter = new SimpleAdapter(this, myList, R.layout.list_row);
+				new String[] {"artist", "title", "place"}, new int[] {R.id.artist, R.id.title, R.id.place};
+				
+				listview.setAdapter(adapter)
+		}
+	}
+	}
+	
+	public void displayData(String Something){
+		
+		if(this.myList != null){
+			this.myList.clear();
+		}
+		String JSONString = DataStorage.readStringFile(getBaseContext(), "my_data.txt");
+		
+		JSONObject job = null;
+		JSONArray recordArray = null;
+		JSONObject field = null;
+		
+		String title;
+		String place;
+		
+		try{
+			job = new JSONObject(JSONString);
+			recordArray = job.getJSONArray("records");
+			
+			for(int i = 0; i < 10 ; i++){
+				field = recordArray.getJSONObject(i).getJSONObject("fields");
+				//Get the items from the Weather API
+				
+				HashMap<String, String> displayMap = new HashMap<String, String>();
+				//PUT the items into the hashmap
+				this.myList.add(displayMap);
+				
+				Log.i("Display Data, JSON record Array"+ i, "artist="+ artist + ", title="+ title+", place="+ place);
+				
+				SimpleAdapter adapter = new SimpleAdapter(this, this.myList, R.layout.list_row, 
+						new String[] {"artist", "title","place"}, new int[]{R.id.artist, R.id.title, R.id.place});
+				
+				listview.setAdapter(adapter);
+				}
+			catch (JSONException e){
+				Log.e("JSON Exception", e.toString());
+				
+			}
+		
+		}
+		@Override
+		
 }
