bca27cf6abf0655efc80527e9be2221a9601e0d3
Dan <Dan@Dinkydetails.com>
2013-09-07 22:44:43 -0400

    Trying to clean up and work on things I do know that are going on!!!

--- WeatherPull/gen/com/dinkydetails/weatherpull/R.java
+++ WeatherPull/gen/com/dinkydetails/weatherpull/R.java
@@ -24,10 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080006;
         public static final int buttonSearch=0x7f080001;
         public static final int editTextZipCode=0x7f080000;
-        public static final int resultTextView=0x7f080002;
+        public static final int tv_actualTemp=0x7f080005;
+        public static final int tv_city=0x7f080002;
+        public static final int tv_feelsLike=0x7f080004;
+        public static final int tv_state=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
