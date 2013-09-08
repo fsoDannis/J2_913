cf7a3c20c6597eff7ee01691eb09d5e3e868fc82
Dan <Dan@Dinkydetails.com>
2013-09-08 08:05:17 -0400

    Fixing Data Storage stuff… Trying something else

--- WeatherPull/src/com/dinkydetails/weatherpull/DataStorage.java
+++ WeatherPull/src/com/dinkydetails/weatherpull/DataStorage.java
@@ -1,124 +1,49 @@
 package com.dinkydetails.weatherpull;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
-import android.content.Context;
-import android.util.Log;
+import android.os.*;
+import android.util.*;
 
 public class DataStorage {
-	//cache data internally
 
-	//creating a history
-	@SuppressWarnings("resource")
-	public static Boolean storeStringFile(Context context, String filename, String content, Boolean external){
-		try{
-			File file;
-			FileOutputStream fos;
-			if(external){
-				file = new File(context.getExternalFilesDir(null), filename);
-				fos = new FileOutputStream(file);
-			}else{
-				fos = context.openFileOutput(filename, Context.MODE_PRIVATE);
-			}
-			//write content
-			fos.write(content.getBytes());
-			fos.close();
-		} catch(IOException e){
-			Log.e("WRITE ERROR", filename);
+	private static final String TAG = DataStorage.class.getSimpleName();
+
+	public static boolean storeStringFile(String filename, String message) {
+		File dir = new File(Environment.getExternalStorageDirectory(), "/download");
+		dir.mkdirs();
+		File file = new File(dir, filename);
+		if (file.exists()) {
+			file.delete();
 		}
+
+		try {
+			FileOutputStream fos = new FileOutputStream(file);
+			fos.write(message.getBytes());
+			fos.close();
+		} catch (Exception e) {
+			Log.e(TAG, e.getMessage());
+			return false;
+		}
+
 		return true;
 	}
-	//store objects on hard drive getting an object as content
-	@SuppressWarnings("resource")
-	public static Boolean storeObjectFile(Context context, String filename, Object content, Boolean external){
-		try{
-			File file;
-			FileOutputStream fos;
-			ObjectOutputStream oos;
-			if(external){
-				file = new File(context.getExternalFilesDir(null), filename);
-				fos = new FileOutputStream(file);
-			}else{
-				fos = context.openFileOutput(filename, Context.MODE_PRIVATE);
+	
+	public static String readStringFile(String filename) {
+		FileInputStream fis = null;
+		StringBuffer fileContent = new StringBuffer("");
+		try {
+			fis = new FileInputStream(Environment.getExternalStorageDirectory() + "/download/" + filename);
+			byte[] buffer = new byte[1024];
+			while (fis.read(buffer) != -1) {
+			    fileContent.append(new String(buffer));
 			}
-			oos = new ObjectOutputStream(fos);
-			//write content
-			oos.writeObject(content);
-			oos.close();
-			fos.close();
-		} catch(IOException e){
-			Log.e("WRITE ERROR", filename);
+			fis.close();
+		} catch (IOException e) {
+			Log.e(TAG, e.getMessage());
 		}
-		return true;
+		
+		return fileContent.toString();
 	}
 
-	@SuppressWarnings("resource")
-	public static String readStringFile(Context context, String filename, Boolean external){
-		String content = "";
-		try{
-			File file;
-			FileInputStream fin;
-			if(external){
-				file = new File(context.getExternalFilesDir(null), filename);
-				fin = new FileInputStream(file);
-			}else{
-				file = new File(filename);
-				fin = context.openFileInput(filename);
-			}
-			BufferedInputStream bin = new BufferedInputStream(fin);
-			byte[] contentBytes = new byte[1024];
-			int bytesRead = 0;
-			StringBuffer contentBuffer = new StringBuffer();
-
-			while((bytesRead = bin.read(contentBytes)) != -1){
-				content = new String(contentBytes,0,bytesRead);
-				contentBuffer.append(content);
-			}
-			content = contentBuffer.toString();
-			fin.close();
-		}catch (FileNotFoundException e){
-			Log.e("READ ERROR", "FILE NOT FOUND" + filename);
-		}catch (IOException e){
-			Log.e("READ ERROR", "I/O ERROR");
-		}
-		return content;
-	}
-	@SuppressWarnings("resource")
-	public static Object readObjectFile(Context context, String filename, Boolean external){
-		Object content = new Object();
-		try{
-			File file;
-			FileInputStream fin;
-			if(external){
-				file = new File(context.getExternalFilesDir(null), filename);
-				fin = new FileInputStream(file);
-			}else{
-				file = new File(filename);
-				fin = context.openFileInput(filename);
-			}
-
-			ObjectInputStream ois = new ObjectInputStream(fin);
-
-			try{
-				content = (Object) ois.readObject();
-			} catch (ClassNotFoundException e){
-				Log.e("READ ERROR", "INVALID JAVA OBJECT FILE");
-			}
-			ois.close();
-			fin.close();
-		}catch (FileNotFoundException e){
-			Log.e("READ ERROR", "FILE NOT FOUND" + filename);
-			return null;
-		}catch (IOException e){
-			Log.e("READ ERROR", "I/O ERROR");
-		}
-		return content;
-	}
 }
