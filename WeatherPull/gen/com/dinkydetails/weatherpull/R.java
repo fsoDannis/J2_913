07133697fd7a85c02ce606ba833dbf0a6e866994
Dan <Dan@Dinkydetails.com>
2013-09-14 21:22:40 -0400

    Getting the UI to be right

--- WeatherPull/gen/com/dinkydetails/weatherpull/R.java
+++ WeatherPull/gen/com/dinkydetails/weatherpull/R.java
@@ -24,16 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
+        public static final int 1day=0x7f080002;
+        public static final int 5day=0x7f080003;
+        public static final int action_settings=0x7f08000b;
         public static final int buttonSearch=0x7f080001;
-        public static final int cloud=0x7f080005;
-        public static final int cloudrow=0x7f080008;
-        public static final int day=0x7f080003;
-        public static final int dayrow=0x7f080006;
+        public static final int cloud=0x7f080007;
+        public static final int cloudrow=0x7f08000a;
+        public static final int day=0x7f080005;
+        public static final int dayrow=0x7f080008;
         public static final int editTextZipCode=0x7f080000;
-        public static final int resultTextView=0x7f080002;
-        public static final int temp=0x7f080004;
-        public static final int temprow=0x7f080007;
+        public static final int list=0x7f080004;
+        public static final int temp=0x7f080006;
+        public static final int temprow=0x7f080009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
