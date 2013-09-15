cc6f594b9ca5206b93d440b011368ef62525b820
Dan <Dan@Dinkydetails.com>
2013-09-15 11:38:32 -0400

    And… its broken again!

--- WeatherPull/gen/com/dinkydetails/weatherpull/R.java
+++ WeatherPull/gen/com/dinkydetails/weatherpull/R.java
@@ -24,21 +24,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
+        public static final int action_settings=0x7f080005;
         public static final int buttonSearch=0x7f080001;
-        public static final int cloud=0x7f080005;
-        public static final int cloudrow=0x7f080008;
-        public static final int day=0x7f080003;
-        public static final int dayrow=0x7f080006;
         public static final int editTextZipCode=0x7f080000;
+        public static final int lblListHeader=0x7f080003;
+        public static final int lblListItem=0x7f080004;
         public static final int resultTextView=0x7f080002;
-        public static final int temp=0x7f080004;
-        public static final int temprow=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int list_header=0x7f030001;
-        public static final int list_row=0x7f030002;
+        public static final int list_group=0x7f030001;
+        public static final int list_item=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
