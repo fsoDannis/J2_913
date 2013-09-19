3c8640bac35faf59b1aea1250be9391ce4dd0be2
Dan <Dan@Dinkydetails.com>
2013-09-18 23:41:33 -0400

    Added a few buttons and my second activity

--- WeatherPull/gen/com/dinkydetails/weatherpull/R.java
+++ WeatherPull/gen/com/dinkydetails/weatherpull/R.java
@@ -24,15 +24,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000b;
+        public static final int action_settings=0x7f08000f;
+        public static final int btnback=0x7f08000c;
         public static final int btnfive=0x7f080005;
+        public static final int btnimplicit=0x7f08000b;
+        public static final int btnnext=0x7f08000a;
         public static final int btnten=0x7f080006;
         public static final int buttonSearch=0x7f080003;
         public static final int editTextZipCode=0x7f080002;
-        public static final int lblListHeader=0x7f080009;
-        public static final int lblListItem=0x7f08000a;
+        public static final int lblListHeader=0x7f08000d;
+        public static final int lblListItem=0x7f08000e;
         public static final int linear1=0x7f080001;
         public static final int linear2=0x7f080004;
+        public static final int linearbottom=0x7f080009;
         public static final int lvExp=0x7f080008;
         public static final int resultTextView=0x7f080007;
         public static final int textheading=0x7f080000;
@@ -49,8 +53,11 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int back=0x7f050007;
         public static final int five=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int implicit=0x7f050006;
+        public static final int next=0x7f050008;
         public static final int prompt_zip_code=0x7f050003;
         public static final int ten=0x7f050005;
     }
